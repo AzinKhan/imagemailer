e8e0502d6ea0c9bbafc7f4e3400fa62fe8a42753
AzinKhan <azink91@googlemail.com>
2018-09-01 18:24:30 +0100

    Prototype emailer
    Writen in starbucks so probably faulty

---
+++ emailer/main.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"bytes"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"net/smtp"
+	"time"
+
+	"github.com/gorilla/mux"
+
+	"github.com/jordan-wright/email"
+)
+
+const addr string = "booboophotomailer@gmail.com"
+const passwd string = "booboo123!"
+
+var toAddress string = "azink91@googlemail.com"
+
+var serverPort string
+
+func init() {
+	flag.StringVar(&serverPort, "p", "8000", "Port for HTTP server")
+	flag.StringVar(&toAddress, "t", toAddress, "Address to send email")
+}
+
+func sendEmail(toAddr string, attachment io.Reader) error {
+	e := email.NewEmail()
+	e.To = []string{toAddr}
+	e.From = addr
+	e.Subject = "Motion detected!"
+	now := fmt.Sprintf("Photo received: %+v", time.Now())
+	e.Text = []byte(now)
+	e.Attach(attachment, fmt.Sprintf("image%+v.jpg", time.Now().Unix()), "image/jpeg")
+	return e.Send("smtp.gmail.com:587", smtp.PlainAuth("", addr, passwd, "smtp.gmail.com"))
+}
+
+type imageChannel chan []byte
+
+func waitForEmail(eChan imageChannel) {
+	for {
+		data := <-eChan
+		err := sendEmail(toAddress, bytes.NewReader(data))
+		if err != nil {
+			log.Printf("Could not send email: %+v", err)
+		}
+	}
+}
+
+func HandlePost(imChan imageChannel) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		r.ParseMultipartForm(100000000)
+		file, _, err := r.FormFile("image")
+		if err != nil {
+			w.WriteHeader(403)
+		} else {
+			defer file.Close()
+			var data []byte
+			file.Read(data)
+			imChan <- data
+			w.WriteHeader(200)
+		}
+	}
+}
+
+func main() {
+	flag.Parse()
+	emailChan := make(imageChannel)
+	address := fmt.Sprintf("0.0.0.0:%v", serverPort)
+	router := mux.NewRouter()
+	router.HandleFunc("/", HandlePost(emailChan))
+	server := &http.Server{
+		Addr:    address,
+		Handler: router,
+	}
+	go waitForEmail(emailChan)
+	log.Fatal(server.ListenAndServe())
+}
