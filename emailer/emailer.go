4e33cce54f645dbd5fed9b54491d0e1a031e8216
AzinKhan <azink91@googlemail.com>
2018-09-05 13:28:32 +0100

    Reorganize package structure and export types

--- emailer/emailer.go
+++ emailer/emailer.go
@@ -1,8 +1,7 @@
-package main
+package emailer
 
 import (
 	"bytes"
-	"flag"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -11,37 +10,14 @@ import (
 	"net/smtp"
 	"time"
 
-	"github.com/gorilla/mux"
 	"github.com/jordan-wright/email"
 )
 
-const addr string = "booboophotomailer@gmail.com"
-const passwd string = "booboo123!"
-
-type addressSlice []string
-
-func (a *addressSlice) Set(value string) error {
-	*a = append(*a, value)
-	return nil
-}
-
-func (a *addressSlice) String() string {
-	return fmt.Sprintf("%s", *a)
-}
-
-var toAddresses addressSlice
-var serverPort string
-
-func init() {
-	flag.StringVar(&serverPort, "p", "8000", "Port for HTTP server")
-	flag.Var(&toAddresses, "t", "Addresses to send email")
-}
-
-type imageChannel chan attachment
+type ImageChannel chan attachment
 
 type Emailer struct {
 	mail   *email.Email
-	imChan imageChannel
+	ImChan ImageChannel
 	passwd string
 }
 
@@ -52,17 +28,17 @@ type attachment struct {
 }
 
 type Creds struct {
-	to       []string
-	from     string
-	password string
+	To       []string
+	From     string
+	Password string
 }
 
 func NewEmailer(c Creds) Emailer {
 	var e Emailer
 	e.mail = email.NewEmail()
-	e.mail.From = c.from
-	e.mail.To = c.to
-	e.passwd = c.password
+	e.mail.From = c.From
+	e.mail.To = c.To
+	e.passwd = c.Password
 	return e
 }
 
@@ -91,7 +67,7 @@ func (e *Emailer) Run() {
 					log.Println("No images received, resetting timer.")
 					t.Reset(20 * time.Second)
 				}
-			case a := <-e.imChan:
+			case a := <-e.ImChan:
 				t.Reset(20 * time.Second)
 				log.Printf("Collecting attachment %+v", a.filename)
 				_, err := e.mail.Attach(bytes.NewReader(a.data), a.filename, a.content)
@@ -142,7 +118,7 @@ func GetForm(r *http.Request) (*multipart.Form, error) {
 	return r.MultipartForm, nil
 }
 
-func HandlePost(imChan imageChannel) func(w http.ResponseWriter, r *http.Request) {
+func HandlePost(imChan ImageChannel) func(w http.ResponseWriter, r *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 		log.Println("Received post")
 		datas, err := GetForm(r)
@@ -163,27 +139,4 @@ func HandlePost(imChan imageChannel) func(w http.ResponseWriter, r *http.Request
 			}
 		}
 	}
-}
-
-func main() {
-	flag.Parse()
-	emailChan := make(imageChannel)
-	address := fmt.Sprintf("0.0.0.0:%v", serverPort)
-	router := mux.NewRouter()
-	router.HandleFunc("/", HandlePost(emailChan))
-	server := &http.Server{
-		Addr:    address,
-		Handler: router,
-	}
-	log.Printf("Email clients are: %+v", toAddresses)
-	credentials := Creds{
-		to:       toAddresses,
-		from:     addr,
-		password: passwd,
-	}
-	emailer := NewEmailer(credentials)
-	emailer.imChan = emailChan
-	go emailer.Run()
-	log.Println("Starting HTTP server..")
-	log.Fatal(server.ListenAndServe())
 }
---
+++ main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"imagemailer/emailer"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+const addr string = "booboophotomailer@gmail.com"
+const passwd string = "booboo123!"
+
+type addressSlice []string
+
+func (a *addressSlice) Set(value string) error {
+	*a = append(*a, value)
+	return nil
+}
+
+func (a *addressSlice) String() string {
+	return fmt.Sprintf("%s", *a)
+}
+
+var toAddresses addressSlice
+var serverPort string
+
+func init() {
+	flag.StringVar(&serverPort, "p", "8000", "Port for HTTP server")
+	flag.Var(&toAddresses, "t", "Addresses to send email")
+}
+
+func main() {
+	flag.Parse()
+	emailChan := make(emailer.ImageChannel)
+	address := fmt.Sprintf("0.0.0.0:%v", serverPort)
+	router := mux.NewRouter()
+	router.HandleFunc("/", emailer.HandlePost(emailChan))
+	server := &http.Server{
+		Addr:    address,
+		Handler: router,
+	}
+	log.Printf("Email clients are: %+v", toAddresses)
+	credentials := emailer.Creds{
+		To:       toAddresses,
+		From:     addr,
+		Password: passwd,
+	}
+	emailer := emailer.NewEmailer(credentials)
+	emailer.ImChan = emailChan
+	go emailer.Run()
+	log.Println("Starting HTTP server..")
+	log.Fatal(server.ListenAndServe())
+}
