e98f4f3628a0b2fc7565a0e027589ed66a098552
AzinKhan <azink91@googlemail.com>
2018-09-10 05:07:09 +0100

    Clean up giffer and add more tests

--- giffer/giffer_test.go
+++ giffer/giffer_test.go
@@ -1,6 +1,8 @@
 package giffer
 
 import (
+	"image"
+	"image/color"
 	"io/ioutil"
 	"log"
 	"path"
@@ -36,6 +38,79 @@ func TestGiffer(t *testing.T) {
 		t.Fail()
 	}
 	if GIF.Len() == 0 {
+		t.Fail()
+	}
+}
+
+func TestGifferReturnsError(t *testing.T) {
+	d := [][]byte{[]byte("This is not a jpeg image")}
+	buf, err := Giffer(d)
+	if err == nil {
+		t.Fail()
+	}
+	if buf != nil {
+		t.Fail()
+	}
+
+}
+
+func TestDecodeJPGError(t *testing.T) {
+	// Make random bytes
+	d := []byte("This is not a jpeg image")
+	img, err := decodeJPG(d)
+	if err == nil {
+		t.Fail()
+	}
+	if img != nil {
+		t.Fail()
+	}
+}
+
+type MockImage struct {
+	Pix string
+}
+
+func (m MockImage) ColorModel() color.Model {
+	log.Println("ColorModel")
+	var c color.Model
+	return c
+}
+
+func (m MockImage) Bounds() image.Rectangle {
+	min := image.Point{
+		X: 0,
+		Y: 0,
+	}
+	max := image.Point{
+		X: 1 << 18,
+		Y: 1 << 18,
+	}
+	r := image.Rectangle{
+		Min: min,
+		Max: max,
+	}
+	return r
+
+}
+
+func (m MockImage) At(x int, y int) color.Color {
+	log.Println("At")
+	var c color.Color
+	return c
+}
+
+func TestConvertToGif(t *testing.T) {
+	// Send in a fake large image
+	m := MockImage{
+		Pix: "hello",
+	}
+	img, err := ConvertToGIF(m)
+	if err == nil {
+		log.Println("Nil error")
+		t.Fail()
+	}
+	if img != nil {
+		log.Println("Image is not nil")
 		t.Fail()
 	}
 
