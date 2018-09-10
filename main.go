2c269f6fb79c67d64d37a18639ff17cb7c3c8081
AzinKhan <azink91@googlemail.com>
2018-09-10 05:08:06 +0100

    Move running logic to main
    The emailing is now executed within its own goroutine, avoiding the
    blocking of the HTTP server.
    It may be worth taking this further and calling the makegif function in
    a goroutine along with writing to the channel.

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"bytes"
 	"flag"
 	"fmt"
 	"imagemailer/emailer"
+	"imagemailer/giffer"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/gorilla/mux"
 )
@@ -27,14 +30,103 @@ func (a *addressSlice) String() string {
 var toAddresses addressSlice
 var serverPort string
 
+type Attachment struct {
+	Data        *bytes.Buffer
+	Filename    string
+	ContentType string
+}
+
+type OutputChan chan *Attachment
+
 func init() {
 	flag.StringVar(&serverPort, "p", "8000", "Port for HTTP server")
 	flag.Var(&toAddresses, "t", "Addresses to send email")
 }
 
+func MakeGIFAttachment(data [][]byte) (*Attachment, error) {
+	GIF, err := giffer.Giffer(data)
+	if err != nil {
+		return &Attachment{}, err
+	}
+	a := Attachment{
+		Data:        GIF,
+		Filename:    fmt.Sprintf("%+v.jpg", time.Now()),
+		ContentType: "image/gif",
+	}
+	return &a, nil
+
+}
+
+func BufferImages(ImChan emailer.ImageChannel, outChan OutputChan) {
+	t := time.NewTimer(20 * time.Second)
+	var data [][]byte
+	size := 0
+	// Either append until memory limit reached or
+	// until timeout
+	for {
+		select {
+		case <-t.C:
+			if size > 0 {
+				log.Println("Timeout reached")
+				GIF, err := MakeGIFAttachment(data)
+				if err != nil {
+					log.Println(err)
+					continue
+				}
+				outChan <- GIF
+				size = 0
+				data = nil
+				t.Reset(20 * time.Second)
+			} else {
+				log.Println("No images received, resetting timer.")
+				t.Reset(20 * time.Second)
+			}
+		case a := <-ImChan:
+			t.Reset(20 * time.Second)
+			log.Printf("Collecting attachment %+v", a.Filename)
+			data = append(data, a.Data)
+			log.Printf("Length of data:\t%+v", len(data))
+			// Write to file
+			size += len(a.Data)
+			log.Printf("Total size: %+v", size)
+			if size >= 2000000 {
+				log.Println("Maximum attachment size reached")
+				GIF, err := MakeGIFAttachment(data)
+				if err != nil {
+					log.Println(err)
+					continue
+				}
+				outChan <- GIF
+				size = 0
+				data = nil
+				t.Reset(20 * time.Second)
+			}
+		}
+	}
+}
+
+func Email(outChan OutputChan, creds emailer.Creds) {
+	for {
+		attachment := <-outChan
+		e := emailer.NewEmailer(creds)
+		_, err := e.Mail.Attach(attachment.Data, attachment.Filename, attachment.ContentType)
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		err = e.Send()
+		if err != nil {
+			log.Printf("Could not send email: %+v", err)
+		} else {
+			log.Println("...done")
+		}
+	}
+}
+
 func main() {
 	flag.Parse()
 	emailChan := make(emailer.ImageChannel)
+	outByteChan := make(OutputChan)
 	address := fmt.Sprintf("0.0.0.0:%v", serverPort)
 	router := mux.NewRouter()
 	router.HandleFunc("/", emailer.HandlePost(emailChan))
@@ -48,9 +140,8 @@ func main() {
 		From:     addr,
 		Password: passwd,
 	}
-	emailer := emailer.NewEmailer(credentials)
-	emailer.ImChan = emailChan
-	go emailer.Run()
+	go Email(outByteChan, credentials)
+	go BufferImages(emailChan, outByteChan)
 	log.Println("Starting HTTP server..")
 	log.Fatal(server.ListenAndServe())
 }
